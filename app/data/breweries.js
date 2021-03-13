9702c8054ec76ef0df2a8ec0c1b1c22e99499b83
Mike Street <mikestreety@gmail.com>
2021-03-13 18:59:48 +0000

    Tweak breweries and data

--- app/data/breweries.js
+++ app/data/breweries.js
@@ -1,4 +1,4 @@
-const Cache = require("@11ty/eleventy-cache-assets");
+const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async function() {
 	let url = "https://beer.mikestreety.co.uk/api/breweries.json";
@@ -11,8 +11,28 @@ module.exports = async function() {
 
 	let data = response.map((brewery) => {
 		brewery.slug = `${brewery.title}`;
+
+		let ratings = brewery.beers
+			.map(beer => {
+				beer.slug = `${beer.title} ${beer.brewery} ${beer.number}`;
+				return beer;
+			})
+			.map(a => Number(a.rating));
+
+		let average = ratings.reduce((a, b) => a + b, 0);
+		average = average / brewery.beers.length;
+		average = Math.round(average * 100) / 100;
+
+		brewery.meta = {
+			average
+		};
+
+		brewery.beers
+			// .sort((a, b) => a.number.localeCompare(b.number))
+			.reverse();
+
 		return brewery;
 	});
 
-	return data;
+	return data.sort((a, b) => a.title.localeCompare(b.title));
 };
