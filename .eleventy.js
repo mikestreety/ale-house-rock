0ecf00f2dc66b5b59e99666aae6a0b049c1b9fe5
Mike Street <mikestreety@gmail.com>
2021-04-07 20:59:08 +0100

    Fix RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
 module.exports = function (config) {
 	// config.addFilter('date', require('./app/filters/date.js'));
 
+	config.addFilter('limit', require('./app/filters/limit.js'));
+	config.addFilter('utcDate', require('./app/filters/utcDate.js'));
 	config.addFilter('isoDate', require('./app/filters/isoDate.js'));
 	config.addFilter('readableDate', require('./app/filters/readableDate.js'));
 	config.addFilter('squashandweed', require('./app/filters/squashandweed.js'));
