6df357324ff7a6b7280444e4ec83d3c7db6a909d
Mike Street <mikestreety@gmail.com>
2026-07-03 20:37:06 +0100

    fix: Add headers to request

--- functions/untappd/untappd.js
+++ functions/untappd/untappd.js
@@ -80,10 +80,20 @@ async function fetchUntappd(pathname) {
 	return fetch(BASE_URL + pathname, {
 		headers: {
 			'User-Agent':
-				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
 			'Accept':
-				'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+				'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 			'Accept-Language': 'en-GB,en;q=0.9',
+			'Accept-Encoding': 'gzip, deflate, br',
+			'Cache-Control': 'max-age=0',
+			'Upgrade-Insecure-Requests': '1',
+			'Sec-Fetch-Dest': 'document',
+			'Sec-Fetch-Mode': 'navigate',
+			'Sec-Fetch-Site': 'none',
+			'Sec-Fetch-User': '?1',
+			'sec-ch-ua': '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
+			'sec-ch-ua-mobile': '?0',
+			'sec-ch-ua-platform': '"Windows"',
 		},
 	});
 }
